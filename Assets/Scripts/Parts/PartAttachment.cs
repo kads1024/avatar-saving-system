fdd1ced3c93507dd36bd9a141e23900429722968
Kenneth Amiel Santos <kenneth.santos@secret6.com>
2022-04-29 17:01:36 +0800

    Setup for applying avatar parts to avatars

---
+++ Assets/Scripts/Parts/PartAttachment.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AvatarSavingSystem
+{
+    /// <summary>
+	/// Class that sets up this part and attaches itself to a parent gameobject
+    /// </summary>
+    public class PartAttachment : MonoBehaviour
+    {
+	
+	}
+}
